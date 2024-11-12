ae504a41d5e1c7a487f8b5f76a503af528702e5b
DaniBelky <belchordaniel123@gmail.com>
2024-11-12 17:10:34 -0300

    Adicionando carrinho em produto OBS: precisa finalizar o js

--- Script/Produto.js
+++ Script/Produto.js
@@ -1,21 +1,24 @@
-// navbar
-function toggleMenu() {
-    var navbar = document.getElementById("navbar");
-    navbar.classList.toggle("ativo");
+let total = 0;
+let itemCount = 0;
+
+function addToCart(productName, productPrice) {
+    const cartItems = document.getElementById('cartItems');
+    const item = document.createElement('li');
+    item.textContent = `${productName} - R$${productPrice}`;
+    cartItems.appendChild(item);
+    const productPriceRegex = productPrice.replace(',', '')
+    total += parseFloat(productPriceRegex).toFixed(3);
+    document.getElementById('totalPrice').textContent = `Total: R$${total.toString().replace(/(\d)(\d{2})$/, "$1,$2")}`;
+
+    // Incrementa a contagem de itens e exibe no ícone do carrinho
+    itemCount++;
+    document.getElementById('cartCount').textContent = itemCount;
 }
 
+function openCart() {
+    document.getElementById('cart').style.display = 'block';
+}
 
-
-//  Buttons
-function showTab(tabName) {
-   
-    const contents = document.querySelectorAll('.content');
-    contents.forEach(content => content.classList.remove('active'));
-
-    const tabs = document.querySelectorAll('.tab');
-    tabs.forEach(tab => tab.classList.remove('active'));
-  
-    document.getElementById(tabName).classList.add('active');
-
-    event.target.classList.add('active');
+function closeCart() {
+    document.getElementById('cart').style.display = 'none';
 }
